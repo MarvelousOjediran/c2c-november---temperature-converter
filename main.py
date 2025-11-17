e32d3332a7fc4db3a839294305998f3264574edb
Marvelous Ojediran <ravenclawrockstar@gmail.com>
2025-11-17 15:21:48 +0000

    added input variables

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+#Create a program that first asks the user which temperature scale they would like to convert from (e.g., Celsius, Fahrenheit, Kelvin). Then, ask which temperature scale they would like to convert to. Afterward, prompt the user for the temperature value and perform the conversion.
 
+#input variable to find out what temperate scale the user wants to use 
+temp_scale = str(input("What temperature scale would you like to start off with? (Celsius, Fahrenheit, Kelvin) "))
+
+temp_convert = str(input("What temperature would you like to cocnvert to? (Celsius, Fahrenheit, Kelvin) "))
